1d75ab71bc6ba88537da73d17be245b958b65000
Amir Diafi <59441376+AmirDiafi@users.noreply.github.com>
2022-07-13 13:09:43 +0100

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by AmirDiafi on 7/13/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
